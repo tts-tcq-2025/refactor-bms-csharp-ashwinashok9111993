23ccbc1205723413e3af186b9f65f8debfb3fff9
ashwinashok9111993 <ashwinashok9111993@gmail.com>
2025-08-05 10:46:02 +0530

    Add CreateMockOutputWriter method to TestHelpers for improved test output handling

--- TestHelpers.cs
+++ TestHelpers.cs
@@ -2,4 +2,10 @@ using System.Collections.Generic;
 
 public static class TestHelpers
 {
+    public static (List<string> capturedOutput, OutputWriter mockWriter) CreateMockOutputWriter()
+    {
+        var capturedOutput = new List<string>();
+        OutputWriter mockWriter = message => capturedOutput.Add(message);
+        return (capturedOutput, mockWriter);
+    }
 }
